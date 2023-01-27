c171f599eb1f5432c43188ec98d3d955c3467f62
krishna <krishnawaghmode1995@gmail.com>
2023-01-27 09:47:15 +0530

    menu changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // $menu = t92::tree();
-        // View::share('menu', $menu);
+        $menu = t92::tree();
+        View::share('menu', $menu);
         date_default_timezone_set('Asia/Kolkata');
     }
 }
